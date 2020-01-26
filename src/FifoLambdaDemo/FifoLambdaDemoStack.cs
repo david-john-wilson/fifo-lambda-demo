dcbd9916733dae43a758ec674f3333781bceefe8
David Wilson <david_john_wilson@hotmail.com>
2020-01-26 20:29:09 +0000

    Demo CloudFormation stack

--- src/FifoLambdaDemo/FifoLambdaDemoStack.cs
+++ src/FifoLambdaDemo/FifoLambdaDemoStack.cs
@@ -1,4 +1,8 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.SQS;
+using Amazon.CDK.AWS.S3;
+using Amazon.CDK.AWS.Lambda;
+using Amazon.CDK.AWS.Lambda.EventSources;
 
 namespace FifoLambdaDemo
 {
@@ -7,6 +11,67 @@ namespace FifoLambdaDemo
         internal FifoLambdaDemoStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
             // The code that defines your stack goes here
+            var sqsDemoError =
+                new Queue
+                (
+                    this,
+                    "DemoError",
+                    new QueueProps
+                    {
+                        QueueName = "demo-error",
+                        RetentionPeriod = Duration.Days(14)
+                    }
+                );
+
+            var sqsDemo =
+                new Queue
+                (
+                    this,
+                    "Demo",
+                    new QueueProps
+                    {                        
+                        QueueName = "demo",
+                        RetentionPeriod = Duration.Days(14),
+                        DeadLetterQueue = 
+                            new DeadLetterQueue
+                            {
+                                MaxReceiveCount = 3,
+                                Queue = sqsDemoError
+                            },
+                        Fifo = true,
+                        ContentBasedDeduplication = false                        
+                    }
+                );
+
+            var s3LambdaCodeBucket =
+                new Bucket
+                (
+                    this,
+                    "DemoLambdaCodeBucket",
+                    new BucketProps
+                    {
+                        BucketName = "lambda-code-bucket"
+                    }
+                );
+
+            var lambdaFunction =
+                new Function
+                (
+                    this,
+                    "DemoLambda",
+                    new FunctionProps
+                    {
+                        Code = Code.FromBucket(s3LambdaCodeBucket, "demo-lambda"),
+                        Handler = "DemoLambda.Handler",
+                        Runtime = Runtime.DOTNET_CORE_2_1,
+                        RetryAttempts = 3,
+                        DeadLetterQueueEnabled = true,
+                        DeadLetterQueue = sqsDemoError,
+                        Timeout = Duration.Seconds(30)
+                    }
+                );
+
+            lambdaFunction.AddEventSource(new SqsEventSource(sqsDemo));
         }
     }
 }
