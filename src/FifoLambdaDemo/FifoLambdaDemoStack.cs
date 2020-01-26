d15189d05a60e0315645ed83696a030e41a80cc7
David Wilson <david_john_wilson@hotmail.com>
2020-01-26 19:21:38 +0000

    Initial commit

---
+++ src/FifoLambdaDemo/FifoLambdaDemoStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace FifoLambdaDemo
+{
+    public class FifoLambdaDemoStack : Stack
+    {
+        internal FifoLambdaDemoStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
